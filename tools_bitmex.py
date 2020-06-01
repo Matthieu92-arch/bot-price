f2d7bb6defb1bd24ee1c497ba3b396a26c74aa27
Matthieu W <matthieu.werth@deepki.com>
2020-06-02 01:02:59 +0200

    asd

--- tools_bitmex.py
+++ tools_bitmex.py
@@ -99,6 +99,12 @@ def reajust_price(entry_price, desired_price, side, quantity, index):
     elif quantity > 750 and side == "Sell":
         return round(((entry_price * 2) / 2) + 0.5 + (index))
 
+    if quantity < -2500 and side == "Sell":
+        return round(desired_price + 10 + (index))
+    elif quantity > 2500 and side == "Buy":
+        return round(desired_price - 10 -(index))
+
+
     if quantity < -2000 and side == "Sell":
         return round(desired_price + 5 + (index))
     elif quantity > 2000 and side == "Buy":
