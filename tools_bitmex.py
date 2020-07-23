60004cd22a75c41270c9fec6bd49da3c9242fa2b
Matthieu W <matthieu.werth@deepki.com>
2020-07-24 00:30:54 +0200

    asd

--- tools_bitmex.py
+++ tools_bitmex.py
@@ -311,6 +311,10 @@ def get_quantity_low(position, index):
     if position < 0:
         cpy = ind.copy()
         cpy.reverse()
+        if index > len(cpy):
+            index = index -1
+        elif index < 0:
+            index = 0
         return round((position * cpy[index]) / 100) * -1
     return round((position * ind[index - 1]) / 100)
 
